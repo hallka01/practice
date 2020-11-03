b7bc2478237d25f10a83cfb2316ce783245115b6
hallka01 <kelley.a.hall@gmail.com>
2020-11-03 13:52:08 -0800

    - fix typos and get basic Flask app running :)

--- test_app/__init__.py
+++ test_app/__init__.py
@@ -9,12 +9,12 @@ def create_app(test_config=None):
     )
     
     if test_config is None:
-        appconfig.from_pyfile('config.py', silent = True)
+        app.config.from_pyfile('config.py', silent = True)
     else:
         app.config.from_mapping(test_config)
     try:
         os.makedirs(app.instance_path)
-    except OSEerror:
+    except OSError:
         pass
     
     @app.route('/hello')
